ee8c93ffed148b0c3eb6c54a626fb203d32cf900
Martin Atkins <matkins@saymedia.com>
2013-11-12 18:07:45 -0800

    setup.py and test requirements.

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="coal",
+    version="dev",
+    packages=['coal'],
+    install_requires=[
+    ]
+)
+
